84b845a3b7969979d0f0df9059e1825fbdf8cfbc
Joshkriti <Komalkanji13@gmail.com>
2024-05-13 16:42:57 +0100

    first push

--- com-Orangehrmlive/src/testsuite/ForgotPasswordTest.java
+++ com-Orangehrmlive/src/testsuite/ForgotPasswordTest.java
@@ -1,4 +1,48 @@
 package testsuite;
 
-public class ForgotPasswordTest {
+import browserfactory.BaseTest;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+
+public class ForgotPasswordTest extends BaseTest {
+
+    String baseUrl = "https://opensource-demo.orangehrmlive.com/";
+
+    @Before
+    public void setupTest(){
+        openBrowser(baseUrl);
+    }
+
+    @Test
+    public void ForgotPasswordTest() {
+    /*
+    1. userShouldNavigateToForgotPasswordPageSuccessfully
+        * click on the ‘Forgot your password’ link
+        * Verify the text ‘Reset Password’
+     */
+        // can't click on forgot password link
+        driver.findElement(By.linkText("Forgot your password?")).click();
+
+        driver.findElement(By.className("oxd-text oxd-text--p orangehrm-login-forgot-header")).click();
+
+        String actualText = driver.findElement(By.xpath("//div[@class='orangehrm-card-container']/form[1]/h6[1]")).getText();
+        String expectingText = "Reset Password";
+        Assert.assertEquals("Print Reset Password: ",actualText,expectingText);
+
+
+
+
+    }
+
+    @After
+    public void tearDown(){
+      //  closeBrowser();
+    }
+
+
+
+
 }
